8fed7e7a27739816b72cdb564dc0391d407bcb69
Beans4276 <beanxy4276@gmail.com>
2026-08-16 19:19:06 -0400

    Change save function to handle JSON data
    Refactor save function to read and write JSON data instead of plain text.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
+import json
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -13,11 +13,18 @@ def save():
     data = request.get_json()
     text = data["text"]
 
-    with open("data.txt", "w") as file:
-        file.write(text)
+    with open("data.json", "r") as file:
+        data = json.load(file)
+        stories = int(data["stories"])
+        library = data["library"]
+        stories += 1
+        library.append(text)
+    with open("data.json", "w") as file:
+        json.dump({"stories": str(stories), "library": library}, file, indent=4)
 
     return jsonify({"success": True})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
